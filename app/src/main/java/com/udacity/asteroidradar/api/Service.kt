f98065b33eb2f6dcbd2f8e13b218fdb873cb5565
Franek <poczta.f@protonmail.com>
2021-02-16 12:11:06 +0100

    Use one Retrofit instance

--- app/src/main/java/com/udacity/asteroidradar/api/Service.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/Service.kt
@@ -10,16 +10,14 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface AsteroidsNeoWsService {
+interface NasaApiService {
     @GET("neo/rest/v1/feed")
     suspend fun getAsteroids(
         @Query("start_date") startDate: String,
         @Query("end_date") endDate: String,
         @Query("api_key") apiKey: String = Constants.API_KEY
     ): String
-}
 
-interface ApodService {
     @GET("planetary/apod")
     suspend fun getPictureOfDay(@Query("api_key") apiKey: String = Constants.API_KEY): PictureOfDay
 }
@@ -29,21 +27,13 @@ private val moshi = Moshi.Builder()
     .build()
 
 object Network {
-    private val scalarsRetrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(Constants.BASE_URL)
-        .build()
-
-    private val moshiRetrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(Constants.BASE_URL)
         .build()
 
-    val asteroidsNeoWsService: AsteroidsNeoWsService by lazy {
-        scalarsRetrofit.create(AsteroidsNeoWsService::class.java)
-    }
-
-    val apodService: ApodService by lazy {
-        moshiRetrofit.create(ApodService::class.java)
+    val nasaApiService: NasaApiService by lazy {
+        retrofit.create(NasaApiService::class.java)
     }
 }
